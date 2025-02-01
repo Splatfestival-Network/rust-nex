44bcad7eda5a020ded984ff02b0c32f24a383f97
DJMrTV <tvnebel@gmail.com>
2025-02-01 20:59:21 +0100

    fix(protocols): fix macro

--- src/protocols/mod.rs
+++ src/protocols/mod.rs
@@ -3,20 +3,27 @@ pub mod server;
 #[macro_export]
 macro_rules! define_protocol {
     ($id:literal $( <$lifetime:lifetime> )?($($varname:ident : $ty:ty),*) => {$($func_id:literal => $func:path),*} ) => {
-        fn protocol $( <$lifetime> )? (rmcmessage: &RMCMessage, $($varname : $ty,)*) -> Option<RMCResponse>{
+        fn protocol $( <$lifetime> )? (rmcmessage: &RMCMessage, $($varname : $ty),*) -> Option<RMCResponse>{
             if rmcmessage.protocol_id != $id{
                 return None;
             }
 
-            let response_result = match rmcmessage.method_id{
+            let response_function = match rmcmessage.method_id{
                 $(
-                    $func_id => $func(rmcmessage),
-                ),*
+                    $func_id => $func,
+                )*
                 _ => {
                     error!("invalid method id sent to protocol {}: {:?}", $id, rmcmessage.method_id);
-                    rmcmessage.error_result_with_code(ErrorCode::Core_NotImplemented)
+                    return Some(
+                        RMCResponse{
+                            protocol_id: $id,
+                            response_result: rmcmessage.error_result_with_code(ErrorCode::Core_NotImplemented)
+                        }
+                    );
                 }
             };
+
+            let response_result = response_function(rmcmessage, $($varname),*);
 
             Some(RMCResponse{
                 protocol_id: $id,
