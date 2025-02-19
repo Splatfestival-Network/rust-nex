6f02339464ec072b282886cfdf33a9261fce905b
DJMrTV <tvnebel@gmail.com>
2025-02-19 23:30:15 +0100

    feat(rmc): start implementing new rmc protocol abstraction

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,11 +1,14 @@
 extern crate proc_macro;
 
-use proc_macro2::TokenTree;
+use proc_macro2::{Ident, Literal, Span, TokenTree};
 use proc_macro::TokenStream;
-
-use syn::{parse_macro_input, DeriveInput, Data};
-use quote::{quote, TokenStreamExt};
-
+use std::iter::FromIterator;
+use syn::{parse_macro_input, DeriveInput, Data, PathSegment, TraitItem, FieldsNamed, Fields, Visibility, Type, TypePath, Path, ImplItem, ImplItemConst, Expr, ExprLit, Lit};
+use quote::{quote, ToTokens, TokenStreamExt};
+use syn::parse::ParseStream;
+use syn::punctuated::Punctuated;
+use syn::spanned::Spanned;
+use syn::Visibility::Public;
 
 /// Example of user-defined [derive mode macro][1]
 ///
@@ -15,8 +18,8 @@ pub fn rmc_serialize(input: TokenStream) -> TokenStream {
     let derive_input = parse_macro_input!(input as DeriveInput);
 
     let struct_attr = derive_input.attrs.iter()
-        .find(|a| a.path.segments.len() == 1 &&
-            a.path.segments.first().is_some_and(|p| p.ident.to_string() == "rmc_struct"));
+        .find(|a| a.path().segments.len() == 1 &&
+            a.path().segments.first().is_some_and(|p| p.ident.to_string() == "rmc_struct"));
 
     let Data::Struct(s) = derive_input.data else {
         panic!("rmc struct type MUST be a struct");
@@ -29,8 +32,8 @@ pub fn rmc_serialize(input: TokenStream) -> TokenStream {
 
         for f in &s.fields{
             if f.attrs.iter()
-                .any(|a| a.path.segments.len() == 1 &&
-                    a.path.segments.first().is_some_and(|p| p.ident.to_string() == "extends")){
+                .any(|a| a.path().segments.len() == 1 &&
+                    a.path().segments.first().is_some_and(|p| p.ident.to_string() == "extends")){
                 continue;
             }
             let ident = f.ident.as_ref().unwrap();
@@ -67,8 +70,8 @@ pub fn rmc_serialize(input: TokenStream) -> TokenStream {
 
         for f in &s.fields{
             if f.attrs.iter()
-                .any(|a| a.path.segments.len() == 1 &&
-                    a.path.segments.first().is_some_and(|p| p.ident.to_string() == "extends")){
+                .any(|a| a.path().segments.len() == 1 &&
+                    a.path().segments.first().is_some_and(|p| p.ident.to_string() == "extends")){
                 continue;
             }
 
@@ -89,23 +92,12 @@ pub fn rmc_serialize(input: TokenStream) -> TokenStream {
     // generate base with extends stuff
 
     let serialize_base_content = if let Some(attr) = struct_attr{
-        let tokens = attr.tokens.clone();
-        let token = tokens.into_iter().next().unwrap();
-
-        let version = match token {
-            TokenTree::Group(g) => {
-                match g.stream().into_iter().next().unwrap(){
-                    TokenTree::Literal(l) => l,
-                    _ => panic!("expected literal")
-                }
-            },
-            _ => panic!("expected group")
-        };
+        let version: Literal = attr.parse_args().expect("has to be a literal");
 
         let pre_inner = if let Some(f) = s.fields.iter().find(|f| {
             f.attrs.iter()
-                .any(|a| a.path.segments.len() == 1 &&
-                    a.path.segments.first().is_some_and(|p| p.ident.to_string() == "extends"))
+                .any(|a| a.path().segments.len() == 1 &&
+                    a.path().segments.first().is_some_and(|p| p.ident.to_string() == "extends"))
         }){
             let ident= f.ident.as_ref().unwrap();
             quote! {
@@ -128,22 +120,12 @@ pub fn rmc_serialize(input: TokenStream) -> TokenStream {
     };
 
     let deserialize_base_content = if let Some(attr) = struct_attr{
-        let tokens = attr.tokens.clone();
-        let token = tokens.into_iter().next().unwrap();
+        let version: Literal = attr.parse_args().expect("has to be a literal");
 
-        let version = match token {
-            TokenTree::Group(g) => {
-                match g.stream().into_iter().next().unwrap(){
-                    TokenTree::Literal(l) => l,
-                    _ => panic!("expected literal")
-                }
-            },
-            _ => panic!("expected group")
-        };
         let pre_inner = if let Some(f) = s.fields.iter().find(|f| {
             f.attrs.iter()
-                .any(|a| a.path.segments.len() == 1 &&
-                    a.path.segments.first().is_some_and(|p| p.ident.to_string() == "extends"))
+                .any(|a| a.path().segments.len() == 1 &&
+                    a.path().segments.first().is_some_and(|p| p.ident.to_string() == "extends"))
         }){
             let ident= f.ident.as_ref().unwrap();
             let ty= &f.ty;
@@ -181,4 +163,134 @@ pub fn rmc_serialize(input: TokenStream) -> TokenStream {
     };
 
     tokens.into()
+}
+
+#[proc_macro_attribute]
+pub fn rmc_proto(attr: TokenStream, input: TokenStream) -> TokenStream{
+    let mut proto_num = parse_macro_input!(attr as syn::LitInt);
+
+    let mut input = parse_macro_input!(input as syn::ItemTrait);
+
+    let info_struct_ident = format!("Raw{}Info",input.ident.to_string());
+    let info_struct_ident = Ident::new(&info_struct_ident, input.ident.span());
+
+    let raw_details_struct = syn::ItemStruct{
+        vis: Visibility::Public(Default::default()),
+        struct_token: Default::default(),
+        fields: Fields::Unit,
+        semi_token: Some(Default::default()),
+        ident: info_struct_ident.clone(),
+        attrs: vec![],
+        generics: Default::default()
+    };
+
+    let raw_details_impl_block = syn::ItemImpl{
+        impl_token: Default::default(),
+        generics: Default::default(),
+        attrs: vec![],
+        brace_token: Default::default(),
+        defaultness: None,
+        trait_: None,
+        self_ty: Box::new(Type::Path(TypePath{
+            qself: None,
+            path: Path{
+                segments: {
+                    let mut punc = Punctuated::new();
+                    punc.push(PathSegment::from(info_struct_ident));
+                    punc
+                },
+                leading_colon: None,
+            }
+        })),
+        unsafety: None,
+        items: vec![
+            ImplItem::Const(
+                ImplItemConst{
+                    defaultness: None,
+                    semi_token: Default::default(),
+                    attrs: vec![],
+                    generics: Default::default(),
+                    ident: Ident::new("PROTOCOL_ID", Span::call_site()),
+                    vis: Public(Default::default()),
+                    colon_token: Default::default(),
+                    const_token: Default::default(),
+                    eq_token: Default::default(),
+                    expr: Expr::Lit(ExprLit{
+                        attrs: vec![],
+                        lit: Lit::Int(proto_num),
+                    }),
+                    ty: Type::Path(TypePath{
+                        qself: None,
+                        path: Path{
+                            segments: {
+                                let mut punc = Punctuated::new();
+                                punc.push(PathSegment::from(Ident::new("u16", Span::call_site())));
+                                punc
+                            },
+                            leading_colon: None,
+                        }
+                    })
+                }
+            )
+        ]
+    };
+
+    let funcs = input.items.iter().filter_map(|v| if let TraitItem::Fn(v) = v {Some(v)} else { None });
+
+    for func in funcs{
+        if matches!(func.default, Some(_)){
+            return syn::Error::new(func.default.span(), "rmc methods may not have bodies").to_compile_error().into();
+        }
+
+        let Some(attr) = func.attrs.iter()
+            .find(|a| a.path().segments.last().is_some_and(|s| s.ident.to_string() == "method_id")) else {
+            let span = func.sig.asyncness.span().join(func.semi_token.unwrap().span()).unwrap_or(func.sig.span());
+            return syn::Error::new(span, "every function inside of an rmc protocol must have a method id").to_compile_error().into();
+        };
+
+        todo!("generate raw impl")
+    }
+
+    quote!{
+        #input
+        #raw_details_struct
+        #raw_details_impl_block
+    }.into()
+
+}
+
+
+#[proc_macro_attribute]
+pub fn method_id(_attr: TokenStream, input: TokenStream) -> TokenStream{
+    // this attribute doesnt do anything by itself, see `rmc_proto`
+    input
+}
+
+
+#[proc_macro_attribute]
+pub fn rmc_struct(attr: TokenStream, input: TokenStream) -> TokenStream{
+    let mut type_data = parse_macro_input!(input as DeriveInput);
+    let mut ident = parse_macro_input!(attr as syn::Path);
+    let last_token = ident.segments.last_mut().expect("empty path?");
+
+    last_token.ident = Ident::new(&("Local".to_owned() + &last_token.ident.to_string()), last_token.span());
+
+
+    let struct_name = &type_data.ident;
+
+    let out = quote!{
+        #type_data
+
+        impl #ident for #struct_name{
+
+        }
+
+        impl crate::rmc::protocols::RmcCallable for #struct_name{
+            async fn rmc_call(&self, protocol_id: u16, method_id: u32, rest: Vec<u8>){
+                <Self as #ident>::rmc_call(self, protocol_id, method_id, rest).await;
+            }
+        }
+    };
+
+    out.into()
 }
--- src/rmc/protocols/mod.rs
+++ src/rmc/protocols/mod.rs
@@ -1,17 +1,109 @@
-use std::sync::Arc;
+use macros::method_id;
+use std::collections::HashMap;
+use std::ops::Add;
+use std::sync::{Arc, Condvar};
+use std::time::Duration;
 use async_trait::async_trait;
-use tokio::sync::Mutex;
-use crate::prudp::socket::ExternalConnection;
+use chrono::TimeDelta;
+use macros::{rmc_proto, rmc_struct};
+use paste::paste;
+use tokio::sync::{Mutex, Notify};
+use tokio::time::{sleep_until, Instant};
+use crate::prudp::socket::{ExternalConnection, SendingConnection};
 use crate::rmc::structures::connection_data::ConnectionData;
 
-pub trait RmcCallable{
-    fn rmc_call(protocol_id: u8, method_id: u8, rest: Vec<u8>);
+struct RmcConnection(SendingConnection, RmcResponseReceiver);
+
+struct RmcResponseReceiver(Notify, Mutex<HashMap<(u16, u32), Vec<u8>>>);
+
+impl RmcResponseReceiver{
+    // returns none if timed out
+    pub async fn get_response_data(&self, proto: u16, method: u32) -> Option<Vec<u8>>{
+        let mut end_wait_time = Instant::now();
+        end_wait_time += Duration::from_secs(5);
+
+        let sleep_fut = sleep_until(end_wait_time);
+        tokio::pin!(sleep_fut);
+
+        loop {
+            let mut locked = self.1.lock().await;
+
+            if let Some(v) = locked.remove(&(proto, method)){
+                return Some(v);
+            }
+
+            let notif_fut = self.0.notified();
+
+            drop(locked);
+
+            tokio::select! {
+                _ = &mut sleep_fut => {
+                    return None;
+                }
+                _ = notif_fut => {
+                    continue;
+                }
+            }
+        }
+    }
 }
 
-struct LocalRmcObjectWrapper<T: RmcCallable>(T);
-impl<T: RmcCallable> LocalRmcObjectWrapper<T>{
-    pub fn new(object: T, conn: ExternalConnection) -> Self{
-        unimplemented!()
+pub trait RmcCallable{
+    async fn rmc_call(&self, protocol_id: u16, method_id: u32, rest: Vec<u8>);
+}
+
+
+macro_rules! define_rmc_proto {
+    (proto $name:ident{
+        $($protocol:path),*
+    }) => {
+        paste!{
+            trait [<Local $name>]: std::any::Any $( + [<Raw $protocol>])* {
+                async fn rmc_call(&self, protocol_id: u16, method_id: u32, rest: Vec<u8>){
+                    match protocol_id{
+                        $(
+                            [<Raw $protocol Info>]::PROTOCOL_ID => <Self as [<Raw $protocol>]>::rmc_call_proto(self, method_id, rest).await,
+                        )*
+                        v => log::error!("invalid protocol called on rmc object {}", v)
+                    }
+                }
+            }
+        }
+    };
+}
+
+trait RawNotif{
+    async fn rmc_call_proto(&self, method_id: u32, rest: Vec<u8>){
+
     }
+}
+
+struct RawNotifInfo;
+impl RawNotifInfo{
+    const PROTOCOL_ID: u16 = 10;
+}
+
+#[rmc_proto(1)]
+pub trait Another{
+    #[method_id(1)]
+    async fn test();
+}
+
+define_rmc_proto!{
+    proto TestProto{
+        Notif,
+        Another
+    }
+}
+
+
+
+#[rmc_struct(TestProto)]
+struct TestProtoImplementor{
+
+}
+
+impl RawNotif for TestProtoImplementor{
+
 }
 
